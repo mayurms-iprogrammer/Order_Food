1b088528c278a05e54a36a5b33487fabd21ff346
mayurms-iprogrammer <mayurms@iprogrammer.com>
2021-04-21 15:58:06 +0530

    updated app

---
+++ src/component/Layout/Header.js
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+import MealsImage from "../../assets/meals.jpg";
+import classes from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
+const Header = (props) => {
+    return (
+        <div>
+            <header className={classes.header}>
+           <h1>Food Mall</h1>
+            <HeaderCartButton/>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={MealsImage} alt="FoodImage" />
+            </div>
+        </div>
+    )
+}
+export default Header;
