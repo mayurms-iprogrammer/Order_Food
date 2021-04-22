1302fc9550d66ebafb5ccd7126a939e85823d797
mayurms-iprogrammer <mayurms@iprogrammer.com>
2021-04-22 18:15:59 +0530

    upto form section

---
+++ src/component/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,15 @@
+import Input from '../../UI/Input';
+import classes from './MealItemForm.module.css';
+
+
+
+const MealItemForm=(props)=>{
+
+return(
+<form className={classes.form}>
+<input/>
+    <button>+ ADD</button>
+</form>
+)
+}
+export default MealItemForm;
